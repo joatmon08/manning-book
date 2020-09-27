debc1d9fb5743c26b563b4bb91a8c4c839368f1e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-09-27 17:22:21 -0400

    Refactor for clearer variables

--- ch02/s01/python/main.py
+++ ch02/s01/python/main.py
@@ -1,51 +1,51 @@
-try:
-    import secrets
-except ImportError:
-    print('"demos/secrets.py" not found.\n\n'
-          'Please copy secrets.py-dist to secrets.py and update the GCE* '
-          'values with appropriate authentication information.\n')
-    sys.exit(1)
-
+import credentials
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
-
-args = getattr(secrets, 'GCE_PARAMS', ())
-kwargs = getattr(secrets, 'GCE_KEYWORD_PARAMS', {})
+from libcloud.common.google import ResourceNotFoundError
 
 
 def get_gce_driver():
-    driver = get_driver(Provider.GCE)(*args, **kwargs)
+    driver = get_driver(Provider.GCE)(
+        credentials.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        credentials.GOOGLE_SERVICE_ACCOUNT_KEY_FILE,
+        project=credentials.GOOGLE_PROJECT,
+        datacenter=credentials.GOOGLE_ZONE)
     return driver
 
 
 def create_server(name, image_family, size):
     driver = get_gce_driver()
     image = driver.ex_get_image_from_family(image_family)
-    node = driver.create_node(name=name, image=image, size=size)
-    return node
+    server = driver.create_node(name=name, image=image, size=size)
+    return server
 
 
 def read_server(name):
     driver = get_gce_driver()
-    node = driver.ex_get_node(name=name)
-    return node
+    server = driver.ex_get_node(name=name)
+    return server
 
 
 def update_server(name, tags):
     driver = get_gce_driver()
-    node = read_server(name)
-    node = driver.ex_set_node_tags(node=node, tags=tags)
-    return node
+    server = read_server(name)
+    server = driver.ex_set_node_tags(node=node, tags=tags)
+    return server
 
 
 def delete_server(name):
     driver = get_gce_driver()
-    node = read_server(name)
-    node = driver.destroy_node(node)
+    server = read_server(name)
+    server = driver.destroy_node(server)
 
 
 if __name__ == "__main__":
     name = 'hello-world'
-    create_server(name, 'ubuntu-1804-lts', 'f1-micro')
-    update_server(name, tags=["dev"])
-    delete_server(name)
+    try:
+        server = read_server(name)
+    except ResourceNotFoundError:
+        server = create_server(name, 'ubuntu-1804-lts', 'f1-micro')
+    print(server)
+
+    # update_server(name, tags=["dev"])
+    # delete_server(name)
