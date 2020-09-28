9a78e8aeedba4f8040302b3daef8439cbf5eaccd
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-09-28 09:28:23 -0400

    Update with end to end checks

--- ch02/s01/python/main.py
+++ ch02/s01/python/main.py
@@ -1,4 +1,5 @@
 import credentials
+from pprint import pprint
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 from libcloud.common.google import ResourceNotFoundError
@@ -26,17 +27,23 @@ def read_server(name):
     return server
 
 
-def update_server(name, tags):
+def update_server(name, image_family=None, labels={}):
     driver = get_gce_driver()
     server = read_server(name)
-    server = driver.ex_set_node_tags(node=node, tags=tags)
-    return server
+    if image_family is not None:
+        new_image = driver.ex_get_image_from_family(image_family)
+        if new_image.name is not server.image:
+            delete_server(name)
+            server = create_server(name, image_family, server.size)
+    status = driver.ex_set_node_labels(server, labels)
+    return status
 
 
 def delete_server(name):
     driver = get_gce_driver()
     server = read_server(name)
-    server = driver.destroy_node(server)
+    status = driver.destroy_node(server)
+    return status
 
 
 if __name__ == "__main__":
@@ -45,7 +52,15 @@ if __name__ == "__main__":
         server = read_server(name)
     except ResourceNotFoundError:
         server = create_server(name, 'ubuntu-1804-lts', 'f1-micro')
-    print(server)
+    print('Server parameters after creation: ' +
+          str(server.extra['labels']) + ' , ' + server.image)
 
-    # update_server(name, tags=["dev"])
-    # delete_server(name)
+    if not update_server(name, image_family='ubuntu-2004-lts',
+                         labels={"environment": "dev"}):
+        raise Exception("Update to server failed")
+
+    server = read_server(name)
+    print('Server parameters after update: ' +
+          str(server.extra['labels']) + ' , ' + server.image)
+
+    delete_server(name)
