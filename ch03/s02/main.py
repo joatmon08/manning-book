e627ea0572bfef27bea74c2afc5911154a38472d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-11-14 10:22:14 -0500

    Singleton code example

---
+++ ch03/s02/main.py
@@ -0,0 +1,33 @@
+import json
+import random
+import os
+from datetime import datetime
+
+
+def google_project_for_reporting_application():
+    name = 'report-application'  #A
+    organization = os.environ.get('USER')  #B
+    project_id = f'{name}-{organization}'  #B
+    return {
+        'resource': [
+            {
+                'google_project': [
+                    {
+                        'reporting_application': [
+                            {
+                                'name': name,
+                                'project_id': project_id
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+
+if __name__ == "__main__":
+    config = google_project_for_reporting_application()  #C
+
+    with open('main.tf.json', 'w') as outfile:  #D
+        json.dump(config, outfile, sort_keys=True, indent=4)  #D
