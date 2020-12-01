fd4e4fc77ba079f74a1cb90d385e44542e8b858b
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-12-01 09:04:56 -0500

    Update ch 3 examples

--- ch03/s01/main.py
+++ ch03/s01/main.py
@@ -1,17 +1,16 @@
 import json
 import os
 
-
-def google_project_for_reporting_application():
-    name = 'report-application'  #A
-    organization = os.environ.get('USER')  #B
-    project_id = f'{name}-{organization}'  #B
+def google_project_for_databases():
+    name = 'databases'  # A
+    organization = os.environ.get('USER')  # B
+    project_id = f'{name}-{organization}'  # B
     return {
         'resource': [
             {
                 'google_project': [
                     {
-                        'reporting_application': [
+                        'databases': [
                             {
                                 'name': name,
                                 'project_id': project_id
@@ -25,7 +24,7 @@ def google_project_for_reporting_application():
 
 
 if __name__ == "__main__":
-    config = google_project_for_reporting_application()  #C
+    config = google_project_for_databases()  # C
 
-    with open('main.tf.json', 'w') as outfile:  #D
-        json.dump(config, outfile, sort_keys=True, indent=4)  #D
+    with open('main.tf.json', 'w') as outfile:  # D
+        json.dump(config, outfile, sort_keys=True, indent=4)  # D
