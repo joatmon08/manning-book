5c974e36f54db2c706c07cc75574584dd8aa775e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-01-05 09:51:21 -0500

    Fix sections in Ch 3

--- ch03/s01/main.py
+++ ch03/s01/main.py
@@ -1,30 +1,49 @@
 import json
-import os
 
-def google_project_for_databases():
-    name = 'databases'  # A
-    organization = os.environ.get('USER')  # B
-    project_id = f'{name}-{organization}'  # B
-    return {
-        'resource': [
-            {
-                'google_project': [
-                    {
-                        'databases': [
-                            {
-                                'name': name,
-                                'project_id': project_id
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+
+class Network:
+    def __init__(self, region='us-central1'):
+        self._network_name = 'my-network'
+        self._network_cidr = '10.0.0.0/16'
+        self._subnet_name = f'{self._network_name}-subnet'
+        self._subnet_cidr = '10.0.0.0/28'
+        self._region = region
+        self.resource = self._build()
+
+    def _build(self):
+        return {
+            'resource': [
+                {
+                    'google_compute_network': [
+                        {
+                            f'{self._network_name}': [
+                                {
+                                    'name': self._network_name
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'google_compute_subnetwork': [
+                        {
+                            f'{self._subnet_name}': [
+                                {
+                                    'name': self._subnet_name,
+                                    'ip_cidr_range': self._subnet_cidr,
+                                    'region': self._region,
+                                    'network': f'${{google_compute_network.{self._network_name}.name}}'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
 
 
 if __name__ == "__main__":
-    config = google_project_for_databases()  # C
+    network = Network()
 
-    with open('main.tf.json', 'w') as outfile:  # D
-        json.dump(config, outfile, sort_keys=True, indent=4)  # D
+    with open(f'main.tf.json', 'w') as outfile:
+        json.dump(network.resource, outfile, sort_keys=True, indent=4)
--- ch03/s02/main.py
+++ ch03/s02/main.py
@@ -1,56 +1,30 @@
 import json
-import netaddr
+import os
 
-
-def get_subnet_address(cidr_range):
-    ip = netaddr.IPNetwork(cidr_range)
-    subnets = list(ip.subnet(28, count=4))
-    return format(subnets[0])
-
-
-class Network:
-    def __init__(self, network='my-network', cidr_range='10.0.0.0/16', region='us-central1'):
-        self._network_name = network
-        self._network_cidr = cidr_range
-        self._subnet_name = f'{network}-subnet'
-        self._subnet_cidr = get_subnet_address(self._network_cidr)
-        self._region = region
-        self.resource = self._build()
-
-    def _build(self):
-        return {
-            'resource': [
-                {
-                    'google_compute_network': [
-                        {
-                            f'{self._network_name}': [
-                                {
-                                    'name': self._network_name
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    'google_compute_subnetwork': [
-                        {
-                            f'{self._subnet_name}': [
-                                {
-                                    'name': self._subnet_name,
-                                    'ip_cidr_range': self._subnet_cidr,
-                                    'region': self._region,
-                                    'network': f'${{google_compute_network.{self._network_name}.name}}'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
+def google_project_for_databases():
+    name = 'databases'  # A
+    organization = os.environ.get('USER')  # B
+    project_id = f'{name}-{organization}'  # B
+    return {
+        'resource': [
+            {
+                'google_project': [
+                    {
+                        'databases': [
+                            {
+                                'name': name,
+                                'project_id': project_id
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
 
 
 if __name__ == "__main__":
-    network = Network()
+    config = google_project_for_databases()  # C
 
-    with open(f'main.tf.json', 'w') as outfile:
-        json.dump(network.resource, outfile, sort_keys=True, indent=4)
+    with open('main.tf.json', 'w') as outfile:  # D
+        json.dump(config, outfile, sort_keys=True, indent=4)  # D
