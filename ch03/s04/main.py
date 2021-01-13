cc9bea347d57f3aa6fb61a01e887b3a937d775b9
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-01-12 21:50:32 -0500

    Refactor prototype

--- ch03/s04/main.py
+++ ch03/s04/main.py
@@ -1,54 +1,65 @@
 import json
 
 
-def standard_tags():
-    return {
-        'customer': 'my-company',
-        'automated': True,
-        'cost_center': 123456,
-        'business_unit': 'ecommerce'
-    }
+class StandardTags():
+    def __init__(self):
+        self.resource = {
+            'customer': 'my-company',
+            'automated': True,
+            'cost_center': 123456,
+            'business_unit': 'ecommerce'
+        }
 
 
-def google_server(name, network, zone='us-central1-a', tags={}):
-    return {
-        'resource': [
-            {
-                'google_compute_instance': [
-                    {
-                        name: [
-                            {
-                                'allow_stopping_for_update': True,
-                                'boot_disk': [
-                                    {
-                                        'initialize_params': [
-                                            {
-                                                'image': 'ubuntu-1804-lts'
-                                            }
-                                        ]
-                                    }
-                                ],
-                                'machine_type': 'f1-micro',
-                                'name': name,
-                                'network_interface': [
-                                    {
-                                        'network': network
-                                    }
-                                ],
-                                'zone': zone,
-                                'labels': tags
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+class GoogleServer:
+    def __init__(self, name, network, zone='us-central1-a', tags={}):
+        self.name = name
+        self.network = network
+        self.zone = zone
+        self.tags = tags
+        self.resource = self._build()
+
+    def _build(self):
+        return {
+            'resource': [
+                {
+                    'google_compute_instance': [
+                        {
+                            self.name: [
+                                {
+                                    'allow_stopping_for_update': True,
+                                    'boot_disk': [
+                                        {
+                                            'initialize_params': [
+                                                {
+                                                    'image': 'ubuntu-1804-lts'
+                                                }
+                                            ]
+                                        }
+                                    ],
+                                    'machine_type': 'f1-micro',
+                                    'name': self.name,
+                                    'network_interface': [
+                                        {
+                                            'network': self.network
+                                        }
+                                    ],
+                                    'zone': self.zone,
+                                    'labels': self.tags
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
 
 
 if __name__ == "__main__":
-    config = google_server(
-        name='hello-world', network='default', tags=standard_tags())
+    config = GoogleServer(
+        name='hello-world', network='default',
+        tags=StandardTags().resource)
 
     with open('main.tf.json', 'w') as outfile:
-        json.dump(config, outfile, sort_keys=True, indent=4)
+        json.dump(config.resource, outfile,
+                  sort_keys=True, indent=4)
