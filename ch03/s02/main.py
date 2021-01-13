3e5ee35917d15a423739ebf03247c2ccd67349b4
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-01-12 21:31:25 -0500

    Update singleton

--- ch03/s02/main.py
+++ ch03/s02/main.py
@@ -1,30 +1,35 @@
 import json
 import os
 
-def google_project_for_databases():
-    name = 'databases'  # A
-    organization = os.environ.get('USER')  # B
-    project_id = f'{name}-{organization}'  # B
-    return {
-        'resource': [
-            {
-                'google_project': [
-                    {
-                        'databases': [
-                            {
-                                'name': name,
-                                'project_id': project_id
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+
+class DatabaseGoogleProject:
+    def __init__(self):
+        self.name = 'databases'  # A
+        self.organization = os.environ.get('USER')  # B
+        self.project_id = f'{self.name}-{self.organization}'  # B
+        self.resource = self._build()
+
+    def _build(self):
+        return {
+            'resource': [
+                {
+                    'google_project': [
+                        {
+                            'databases': [
+                                {
+                                    'name': self.name,
+                                    'project_id': self.project_id
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
 
 
 if __name__ == "__main__":
-    config = google_project_for_databases()  # C
+    project = DatabaseGoogleProject()  # C
 
     with open('main.tf.json', 'w') as outfile:  # D
-        json.dump(config, outfile, sort_keys=True, indent=4)  # D
+        json.dump(project.resource, outfile, sort_keys=True, indent=4)  # D
