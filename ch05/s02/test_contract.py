88f6157ec6d5471b0ac809583ddf9813788ff0d5
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-01-30 16:37:09 -0500

    Update contract tests

--- ch05/s02/test_contract.py
+++ ch05/s02/test_contract.py
@@ -1,11 +1,25 @@
-from network import NetworkFactoryModule
+from network import NetworkFactoryModule, NetworkFacade
+import pytest
+
+network_name = 'hello-world'
+network_cidr_range = '10.0.0.0/16'
 
 
-def test_network_outputs_contains_name_and_ip_address_for_server():
-    network_name = 'hello-world'
-    network_cidr_range = '10.0.0.0/16'
+@pytest.fixture
+def network_outputs():
     network = NetworkFactoryModule(
-        name=network_name, ip_range=network_cidr_range)
+        name=network_name,
+        ip_range=network_cidr_range)
+    return network.outputs()
 
-    assert network.outputs()._network == f"{network_name}-subnet"
-    assert network.outputs()._ip_cidr_range == network_cidr_range
+
+def test_network_output_is_facade(network_outputs):
+    assert isinstance(network_outputs, NetworkFacade)
+
+
+def test_network_output_has_network_name(network_outputs):
+    assert network_outputs._network == f"{network_name}-subnet"
+
+
+def test_network_output_has_ip_cidr_range(network_outputs):
+    assert network_outputs._ip_cidr_range == network_cidr_range
