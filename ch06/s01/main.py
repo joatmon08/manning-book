5cffd77bb3a7a66526a0337ad58a75c31ca64196
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-03-26 09:49:11 -0400

    Add unit tests

---
+++ ch06/s01/main.py
@@ -0,0 +1,62 @@
+import json
+import os
+import version
+
+SERVICE_CONFIGURATION_FILE = 'main.tf.json'
+SERVICE_IMAGE = 'us-docker.pkg.dev/cloudrun/container/hello'
+
+
+class GoogleCloudRunFactoryModule:
+    def __init__(self, name, image, version,
+                 location='us-central1'):
+        self._name = name
+        self._location = location
+        self._image = f'{image}:{version}'
+        self.resources = self._build()
+
+    def _build(self):
+        return {
+            'resource': [{
+                'google_cloud_run_service': [{
+                    self._name: [{
+                        'name': self._name,
+                        'location': self._location,
+                        'template': {
+                            'spec': {
+                                'containers': {
+                                    'image': self._image
+                                }
+                            }
+                        },
+                        'traffic': {
+                            'percent': 100,
+                            'latest_revision': True
+                        }
+                    }]
+                }],
+                'google_cloud_run_service_iam_member': [{
+                    self._name: [{
+                        'service': f"${{google_cloud_run_service.{self._name}.name}}",
+                        'location': f"${{google_cloud_run_service.{self._name}.location}}",
+                        'role': 'roles/run.invoker',
+                        'member': 'allUsers'
+                    }]
+                }]
+            }],
+            'output': {
+                'url': {
+                    'value': f"${{google_cloud_run_service.{self._name}.status[0].url}}"
+                }
+            }
+        }
+
+
+def generate_json(service_name):
+    service = GoogleCloudRunFactoryModule(
+        name=service_name,
+        image=SERVICE_IMAGE,
+        version=version.HELLO
+    )
+    with open(SERVICE_CONFIGURATION_FILE, 'w') as outfile:
+        json.dump(service.resources, outfile,
+                  sort_keys=True, indent=4)
---
+++ ch06/s01/test_service.py
@@ -0,0 +1,51 @@
+import json
+import os
+import pytest
+from main import generate_json, SERVICE_IMAGE, SERVICE_CONFIGURATION_FILE
+import version
+
+expected_service_name = 'hello-world'
+
+
+@pytest.fixture(scope="module")
+def configuration():
+    generate_json(expected_service_name)
+    with open(SERVICE_CONFIGURATION_FILE, 'r') as f:
+        yield json.load(f)
+    if os.path.exists(SERVICE_CONFIGURATION_FILE):
+        os.remove(SERVICE_CONFIGURATION_FILE)
+
+
+@pytest.fixture
+def resource():
+    def _get_resource(configuration, resource_type):
+        for resource in configuration['resource']:
+            if resource_type in resource.keys():
+                return resource[resource_type]
+    return _get_resource
+
+
+@pytest.fixture
+def service(configuration, resource):
+    return resource(
+        configuration,
+        'google_cloud_run_service'
+    )[0][expected_service_name][0]
+
+
+@pytest.fixture
+def service_iam_member(configuration, resource):
+    return resource(
+        configuration,
+        'google_cloud_run_service_iam_member'
+    )
+
+
+def test_configuration_for_service_name(service):
+    assert service['name'] \
+        == expected_service_name
+
+
+def test_configuration_for_service_version(service):
+    assert service['template']['spec']['containers']['image'] \
+        == f'{SERVICE_IMAGE}:{version.HELLO}'
