4938b5dc0be8e9b43bd1e408f2ab45a7d9bbec55
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-07-16 12:04:36 -0400

    Add passive example

--- ch09/main.py
+++ ch09/main.py
@@ -1,11 +1,12 @@
 import blue
+import passive
 import shared
 import json
 
 if __name__ == "__main__":
-    resources = {
-        'resource': blue.build() + shared.build() 
-    }
+    resources = {'resource': blue.build() +
+                 passive.build() + shared.build()}
 
     with open('main.tf.json', 'w') as outfile:
-        json.dump(resources, outfile, sort_keys=True, indent=4)
+        json.dump(resources, outfile,
+                  sort_keys=True, indent=4)
--- ch09/shared.py
+++ ch09/shared.py
@@ -1,4 +1,5 @@
 import blue
+import passive
 
 TEAM = 'sundew'
 COMPANY = 'dc4plants'
@@ -7,16 +8,42 @@ PORT = 8080
 
 shared_name = f'{TEAM}-{ENVIRONMENT}-shared'
 
-def build():
-    return load_balancer()
+default_version = 'blue'
 
-def load_balancer(name=shared_name,
-                  blue_zone=blue.zone):
-    return [{
+services_list = [
+    {
+        'version': 'blue',
+        'zone': blue.zone,
+        'name': f'{shared_name}-blue',
+        'weight': 100
+    }, {
+        'version': 'passive',
+        'zone': passive.zone,
+        'name': f'{shared_name}-passive',
+        'weight': 0
+    }
+]
+
+
+def build():
+    return backend_services(services_list) + \
+        load_balancer(shared_name,
+                      default_version,
+                      services_list)
+
+
+def backend_services(services):
+    backend_services = []
+    for service in services:
+        name = service['name']
+        version = service['version']
+        zone = service['zone']
+        backend_services.append({
             'google_compute_backend_service': {
-                TEAM: [{
+                version: [{
                     'backend': [{
-                        'group': '${google_compute_instance_group.blue.self_link}'
+                        'group': ('${google_compute_instance_group.'
+                                  f'{version}.self_link}}')
                     }],
                     'health_checks': [
                         f'${{google_compute_health_check.{TEAM}.id}}'
@@ -27,66 +54,100 @@ def load_balancer(name=shared_name,
                     'provider': 'google-beta'
                 }]
             },
-            'google_compute_global_forwarding_rule': {
-                TEAM: [{
-                    'name': name,
-                    'port_range': '80',
-                    'target': f'${{google_compute_target_http_proxy.{TEAM}.id}}'
-                }]
-            },
-            'google_compute_health_check': {
-                TEAM: [{
-                    'http_health_check': [{
-                        'port': PORT
-                    }],
-                    'name': name,
-                    'provider': 'google-beta'
-                }]
-            },
             'google_compute_instance_group': {
-                'blue': [{
-                    'description': 'Blue server instance group',
+                version: [{
+                    'description': f'{version} server instance group',
                     'instances': [
-                        '${google_compute_instance.blue_0.id}',
-                        '${google_compute_instance.blue_1.id}',
-                        '${google_compute_instance.blue_2.id}'
+                        f'${{google_compute_instance.{version}_0.id}}',
+                        f'${{google_compute_instance.{version}_1.id}}',
+                        f'${{google_compute_instance.{version}_2.id}}'
                     ],
-                    'name': blue.server_name,
+                    'name': name,
                     'named_port': [{
                         'name': 'http',
                         'port': f'{PORT}'
                     }],
-                    'zone': blue_zone
-                }]
-            },
-            'google_compute_target_http_proxy': {
-                TEAM: [{
-                    'description': f'Target proxy for {TEAM}',
-                    'name': name,
-                    'url_map': f'${{google_compute_url_map.{TEAM}.id}}'
-                }]
-            },
-            'google_compute_url_map': {
-                TEAM: [{
-                    'default_service': f'${{google_compute_backend_service.{TEAM}.id}}',
-                    'description': f'URL Map for {TEAM}',
-                    'host_rule': [{
-                        'hosts': [
-                            f'{TEAM}.{COMPANY}.com'
-                        ],
-                        'path_matcher': 'allpaths'
-                    }],
-                    'name': name,
-                    'path_matcher': [{
-                        'default_service': f'${{google_compute_backend_service.{TEAM}.id}}',
-                        'name': 'allpaths',
-                        'path_rule': [{
-                                'paths': [
-                                    '/*'
-                                ],
-                            'service': f'${{google_compute_backend_service.{TEAM}.id}}'
-                        }]
-                    }]
+                    'zone': zone
                 }]
             }
+        })
+    return backend_services
+
+
+def _generate_backend_services(services):
+    backend_services_list = []
+    for service in services:
+        version = service['version']
+        weight = service['weight']
+        backend_services_list.append({
+            'backend_service': (
+                '${google_compute_backend_service.'
+                f'{version}.id}}'
+            ),
+            'weight': weight,
+        })
+    return backend_services_list
+
+
+def load_balancer(name, default_version, services):
+    return [{
+        'google_compute_global_forwarding_rule': {
+            TEAM: [{
+                'name': name,
+                'port_range': '80',
+                'target': ('${google_compute_target_http_proxy.'
+                           f'{TEAM}.id}}'),
+                'load_balancing_scheme': 'INTERNAL_SELF_MANAGED',
+                'ip_address': '0.0.0.0'
             }]
+        },
+        'google_compute_health_check': {
+            TEAM: [{
+                'http_health_check': [{
+                    'port': PORT
+                }],
+                'name': name,
+                'provider': 'google-beta'
+            }]
+        },
+        'google_compute_target_http_proxy': {
+            TEAM: [{
+                'description': f'Target proxy for {TEAM}',
+                'name': name,
+                'url_map': f'${{google_compute_url_map.{TEAM}.id}}'
+            }]
+        },
+        'google_compute_url_map': {
+            TEAM: [{
+                'default_service': (
+                    '${google_compute_backend_service.'
+                    f'{default_version}.id}}'
+                ),
+                'description': f'URL Map for {TEAM}',
+                'host_rule': [{
+                    'hosts': [
+                        f'{TEAM}.{COMPANY}.com'
+                    ],
+                    'path_matcher': 'allpaths'
+                }],
+                'name': name,
+                'path_matcher': [{
+                    'default_service': (
+                        '${google_compute_backend_service.'
+                        f'{default_version}.id}}'
+                    ),
+                    'name': 'allpaths',
+                    'path_rule': [{
+                        'paths': [
+                            '/*'
+                        ],
+                        'route_action': {
+                            'weighted_backend_services':
+                                _generate_backend_services(
+                                    services)
+                        }
+                    }]
+                }]
+            }]
+        }
+    }]
