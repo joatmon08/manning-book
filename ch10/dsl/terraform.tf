eaf9c2da23c313e1764a057d44e6f7637c4520fd
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-08-10 10:38:28 -0400

    Add DSL examples

---
+++ ch10/dsl/terraform.tf
@@ -0,0 +1,10 @@
+variable "enable_server_module" {
+  type        = bool
+  default     = false
+  description = "Choose true to build servers with a module."
+}
+
+module "server" {
+  count   = var.enable_server_module ? 1 : 0
+  ## omitted for clarity
+}
